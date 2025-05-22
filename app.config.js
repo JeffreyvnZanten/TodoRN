96aebb3a1852a13f079810a47dccb8673122ac27
jeffreyvnzanten <j.van.zanten@gmail.com>
2025-05-22 13:11:45 +0200

    Hardcoded credentials moved to env aswell

--- app.config.js
+++ app.config.js
@@ -3,5 +3,7 @@ export default ({ config }) => ({
   extra: {
     API_BASE_URL: process.env.API_BASE_URL,
     BASE_URL: process.env.BASE_URL,
+    AUTH_EMAIL: process.env.AUTH_EMAIL,
+    AUTH_PASSWORD: process.env.AUTH_PASSWORD,
   },
 });
--- services/loginService.ts
+++ services/loginService.ts
@@ -1,10 +1,16 @@
+import Constants from "expo-constants";
 import { authClient } from "../auth/auth-client";
+
+const { AUTH_EMAIL, AUTH_PASSWORD } = Constants.expoConfig!.extra as {
+  AUTH_EMAIL: string;
+  AUTH_PASSWORD: string;
+};
 
 export const performLogin = async () => {
   try {
     const { data, error } = await authClient.signIn.email({
-      email: "test@example.com",
-      password: "veiligWachtwoord123",
+      email: AUTH_EMAIL,
+      password: AUTH_PASSWORD,
     });
 
     if (error) {
@@ -13,7 +19,7 @@ export const performLogin = async () => {
     }
 
     console.log("Ingelogd als:", data.user);
-  } catch (err: any) {
-    console.error("Verbindingsfout of andere error:", err);
+  } catch (error: any) {
+    console.error("Verbindingsfout of andere error:", error);
   }
 };
